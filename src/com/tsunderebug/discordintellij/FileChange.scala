9c4d1ac4e570d296af987241ebb3a462c3dd467a
Anthony Cerruti <tsunderebug@tsunderebug.com>
2017-11-26 00:22:00 -0800

    Fix bug

--- src/com/tsunderebug/discordintellij/FileChange.scala
+++ src/com/tsunderebug/discordintellij/FileChange.scala
@@ -6,7 +6,7 @@ import com.tsunderebug.drpc.RichPresence
 class FileChange extends FileEditorManagerListener {
 
   override def selectionChanged(event: FileEditorManagerEvent): Unit = {
-    val lowercaseName = event.getNewFile.getFileType.getName.take(event.getNewFile.getFileType.getName.indexOf(' ')).toLowerCase
+    val lowercaseName = event.getNewFile.getFileType.getName.dropRight(Math.max(event.getNewFile.getFileType.getName.indexOf(' ') + 1, 0)).toLowerCase
     RichPresence(
       state = s"Working on ${event.getManager.getProject.getName}",
       largeImageKey = lowercaseName,
