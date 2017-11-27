bfc36cb110c55192a4c4da65db9b685dd976bb59
Anthony Cerruti <tsunderebug@tsunderebug.com>
2017-11-26 20:58:10 -0800

    Bugfix for NPE

--- src/com/tsunderebug/discordintellij/FileChange.scala
+++ src/com/tsunderebug/discordintellij/FileChange.scala
@@ -7,18 +7,28 @@ import com.tsunderebug.drpc.RichPresence
 class FileChange extends FileEditorManagerListener {
 
   override def selectionChanged(event: FileEditorManagerEvent): Unit = {
-    val name = try event.getNewFile.getFileType.getName catch {case _: NullPointerException => "Text"}
-    val lowercaseName = name.split("""\s""").head.toLowerCase
-    val ide = ApplicationInfo.getInstance.getBuild.asString.substring(0, 2).toLowerCase
-    RichPresence(
-      state = s"Working on ${event.getManager.getProject.getName}",
-      largeImageKey = lowercaseName,
-      largeImageText = s"Editing a $name file",
-      smallImageKey = ide,
-      smallImageText = s"Using IntelliJ version ${ApplicationInfo.getInstance.getBuild.asString}",
-      details = s"Editing [$name] ${event.getNewFile.getName}",
-      startTimestamp = System.currentTimeMillis() / 1000
-    ).submit()
+    Option(event.getNewFile) match {
+      case Some(file) =>
+        val name = file.getFileType.getName
+        val lowercaseName = name.split("""\s""").head.toLowerCase
+        val ide = ApplicationInfo.getInstance.getBuild.asString.substring(0, 2).toLowerCase
+        RichPresence(
+          state = s"Working on ${event.getManager.getProject.getName}",
+          largeImageKey = lowercaseName,
+          largeImageText = s"Editing a $name file",
+          smallImageKey = ide,
+          smallImageText = s"Using IntelliJ version ${ApplicationInfo.getInstance.getBuild.asString}",
+          details = s"Editing [$name] ${file.getName}",
+          startTimestamp = System.currentTimeMillis() / 1000
+        ).submit()
+      case None =>
+        RichPresence(
+          state = s"Idling",
+          details = s"In ${event.getManager.getProject.getName}",
+          startTimestamp = System.currentTimeMillis() / 1000
+        ).submit()
+    }
+
   }
 
 }
