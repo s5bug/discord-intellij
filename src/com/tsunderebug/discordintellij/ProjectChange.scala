e46210ee02efd98a033004252128fa189ba0960c
Anthony Cerruti <tsunderebug@tsunderebug.com>
2017-11-27 18:58:53 -0800

    RP now shows up even when not in a project.

--- src/com/tsunderebug/discordintellij/ProjectChange.scala
+++ src/com/tsunderebug/discordintellij/ProjectChange.scala
@@ -1,13 +1,20 @@
 package com.tsunderebug.discordintellij
 
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.tsunderebug.drpc.RichPresence
 
 class ProjectChange extends StartupActivity {
 
   override def runActivity(project: Project): Unit = {
-    project.getMessageBus.connect().subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileChange)
+    val hook = new FileChange
+    project.getMessageBus.connect().subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, hook)
+    RichPresence(
+      state = s"Opened ${project.getName}",
+      details = s"${ApplicationInfo.getInstance().getApiVersion}"
+    ).submit()
   }
 
 }
--- src/com/tsunderebug/discordintellij/StartupShutdown.scala
+++ src/com/tsunderebug/discordintellij/StartupShutdown.scala
@@ -1,5 +1,6 @@
 package com.tsunderebug.discordintellij
 
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.components.ApplicationComponent
 import com.tsunderebug.drpc.RichPresence
 
@@ -7,6 +8,23 @@ class StartupShutdown extends ApplicationComponent {
 
   override def initComponent(): Unit = {
     RichPresence.start("384215522050572288")
+    RichPresence(
+      state = s"In IDE ${getIDEName(ApplicationInfo.getInstance().getBuild.asString())}",
+      details = s"${ApplicationInfo.getInstance().getApiVersion}"
+    ).submit()
+  }
+
+  def getIDEName(bn: String): String = {
+    val code = bn.take(2).toLowerCase
+    code match {
+      case "ic" | "iu" => "IntelliJ"
+      case "py" => "PyCharm"
+      case "rm" => "RubyMine"
+      case "go" => "GoLand"
+      case "cl" => "CLion"
+      case "ps" => "PhpStorm"
+      case "ws" => "WebStorm"
+    }
   }
 
   override def disposeComponent(): Unit = RichPresence.stop()
