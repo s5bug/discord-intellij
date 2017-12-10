c71c14233e8183d6f90a90b3aa050057086cc904
Jonathan Colmenares <jonathan@yolouso.com>
2017-12-10 16:31:30 -0500

    Added support for more IDEs
    AppCode (a macOS exclusive IDE), MPS, DataGrip, and Rider.

--- src/com/tsunderebug/discordintellij/package.scala
+++ src/com/tsunderebug/discordintellij/package.scala
@@ -4,13 +4,17 @@ package object discordintellij {
 
   def getIDEName(code: String): String = {
     code match {
-      case "ic" | "iu" => "IntelliJ"
+      case "ic" | "iu" => "IntelliJ IDEA"
       case "py" | "pc" | "pe" => "PyCharm"
       case "rm" => "RubyMine"
       case "go" => "GoLand"
       case "cl" => "CLion"
       case "ps" => "PhpStorm"
       case "ws" => "WebStorm"
+      case "ac" => "AppCode"
+      case "mp" => "MPS"
+      case "db" => "DataGrip"
+      case "rd" => "Rider"
       case _ => s"[Unknown IDE ${code.toUpperCase}]"
     }
   }
