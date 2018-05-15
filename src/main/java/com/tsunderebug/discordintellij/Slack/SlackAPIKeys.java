7918ea565a579a733f6c5ddf7cfce7b52ee0aecc
Stephen P. Cerruti <scerruti@otabi.com>
2018-05-14 20:56:23 -0700

    Hiding API Keys

---
+++ src/main/java/com/tsunderebug/discordintellij/Slack/SlackAPIKeys.java
@@ -0,0 +1,13 @@
+package com.tsunderebug.discordintellij.Slack;
+
+public static class SlackAPIKeys {
+#
+    error You
+    need to
+    set up
+    keys.
+    // This file is excluded from GitHub. You must update this file to build.
+    public static String SLACK_OAUTH_CLIENT_ID = "<omitted>";
+    your API
+    public static String SLACK_OAUTH_CLIENT_SECRET = "<omitted>";
+}
