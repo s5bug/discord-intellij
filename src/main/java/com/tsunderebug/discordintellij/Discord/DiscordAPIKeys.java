f352e71039485ba54120ef7c274bebeae26724a9
Stephen Cerruti <scerruti@otabi.com>
2018-05-16 12:31:34 -0700

    Update DiscordAPIKeys.java
    Fix reformatting done by commit.

--- src/main/java/com/tsunderebug/discordintellij/Discord/DiscordAPIKeys.java
+++ src/main/java/com/tsunderebug/discordintellij/Discord/DiscordAPIKeys.java
@@ -1,11 +1,7 @@
 package com.tsunderebug.discordintellij.Discord;
 
 public class DiscordAPIKeys {
-#
-    error You
-    need to
-    set up
-    keys.
-            your API    // This file is excluded from GitHub. You must update this file to build.
+#error You need to set up your API keys.
+    // This file is excluded from GitHub. You must update this file to build.
     public static String DISCORD_CLIENT_ID = "<omitted>";
 }
